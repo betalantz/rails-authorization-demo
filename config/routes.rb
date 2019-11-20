8cd9e1301801e0b30a09d69b24cf156710705011
betalantz <betalantz@gmail.com>
2019-11-19 17:40:19 -0800

    add signup route and form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :journal_entries
-  resources :users
+  resources :users, except: [:new]
+
+  get '/signup', to: 'users#new', as: 'signup'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
