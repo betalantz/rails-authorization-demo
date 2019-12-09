4141d91766b70fc7e925983893a2540f8dceb094
betalantz <betalantz@gmail.com>
2019-12-08 23:06:02 -0800

    adds authorize helper method and error module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
 class ApplicationController < ActionController::Base
     helper_method :logged_in?
     helper_method :current_user
+    helper_method :owns_resource?
+
+    rescue_from ActiveRecord::RecordNotFound, :with => :rescue404
+    rescue_from ActionController::RoutingError, :with => :rescue404
+    rescue_from ActionController::InvalidAuthenticityToken, :with => :rescue403
+    rescue_from Errors::AuthorizationError, :with => :rescue403
+
+    def rescue404
+        render(:file => File.join(Rails.root, 'public/404.html'), :status => 404, :layout => false)
+    end
+
+    def rescue403
+        render(:file => File.join(Rails.root, 'public/403.html'), :status => 403, :layout => false)
+    end
 
     private
      
@@ -14,6 +28,14 @@ class ApplicationController < ActionController::Base
 
     def authenticate
         redirect_to login_path unless logged_in?
+    end
+
+    def owns_resource?(resource)
+        resource.user == current_user
+    end
+
+    def authorize(resource)
+        raise Errors::AuthorizationError.new if !owns_resource?(resource)
     end
 
 end
--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -1,19 +1,46 @@
 class JournalEntriesController < ApplicationController
     before_action :authenticate
-    before_action :get_journal_entry, only: [:edit]
+    before_action :get_journal_entry, only: [:edit, :update]
     
 
     def index
         @journal_entries = JournalEntry.all
     end
 
-    def edit
     
+    def edit
+        authorize(@journal_entry)
+    end
+    
+    def new
+        @journal_entry = JournalEntry.new
+    end
+    
+    def create
+        journal_entry = current_user.journal_entries.build(journal_entry_params)
+        if journal_entry.save
+            flash[:success] = "Your entry was successfully created!"
+            redirect_to journal_entries_path 
+        else
+            flash[:error] = "Something went wrong saving your entry!"
+            redirect_to new_journal_entry_path
+        end
+    end
+    
+    def update
+        authorize(@journal_entry)
+        @journal_entry.update(journal_entry_params)
+        flash[:success] = "Your journal entry was updated!ß"
+        redirect_to journal_entries_path
     end
 
     private
 
     def get_journal_entry
         @journal_entry = JournalEntry.find(params[:id])
+    end
+
+    def journal_entry_params
+        params.require(:journal_entry).permit(:title, :content, :user_id)
     end
 end
