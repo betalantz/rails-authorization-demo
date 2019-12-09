681ce574200ae88b7f2e8e8da059560c29f6ee30
betalantz <betalantz@gmail.com>
2019-12-08 22:07:46 -0800

    adds views and partial for new/update

--- app/controllers/journal_entries_controller.rb
+++ app/controllers/journal_entries_controller.rb
@@ -1,8 +1,19 @@
 class JournalEntriesController < ApplicationController
     before_action :authenticate
-
+    before_action :get_journal_entry, only: [:edit]
+    
 
     def index
         @journal_entries = JournalEntry.all
+    end
+
+    def edit
+    
+    end
+
+    private
+
+    def get_journal_entry
+        @journal_entry = JournalEntry.find(params[:id])
     end
 end
